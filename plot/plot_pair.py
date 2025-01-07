a7d849327cadab1b51f134a37cd95ddb04f224b2
kasra <kasra.research@gmail.com>
2025-01-07 00:00:18 -0500

    Add noise calculation

--- plot/plot_pair.py
+++ plot/plot_pair.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import glob
+from matplotlib import rcParams
 
+# Set Times New Roman as the default font
 def plot_multiple(title,file_names):
     plt.figure(figsize=(10, 6))
 
@@ -24,24 +26,27 @@ def plot_multiple(title,file_names):
         plt.plot(rounds, accuracy, marker='o', linestyle='-', color=colors[idx % len(colors)], label=f'{label}')
 
         # Mark the maximum accuracy
-        plt.scatter(max_accuracy_round, max_accuracy, color=colors[idx % len(colors)], s=100, zorder=5)
-        plt.annotate(
-            f'({max_accuracy_round}, {max_accuracy:.2f})',
-            (max_accuracy_round, max_accuracy),
-            textcoords="offset points",
-            xytext=(0, 10),
-            ha='center',
-            fontsize=10,
-            color=colors[idx % len(colors)]
-        )
+        # plt.scatter(max_accuracy_round, max_accuracy, color=colors[idx % len(colors)], s=100, zorder=5)
+        # plt.annotate(
+        #     f'({max_accuracy_round}, {max_accuracy:.2f})',
+        #     (max_accuracy_round, max_accuracy),
+        #     textcoords="offset points",
+        #     xytext=(0, 10),
+        #     ha='center',
+        #     fontsize=10,
+        #     color=colors[idx % len(colors)]
+        # )
 
     # Add title, labels, and legend
-    plt.ylim(0.3, 1)
-    plt.title(title, fontsize=14)
-    plt.xlabel("Round", fontsize=12)
-    plt.ylabel("Accuracy", fontsize=12)
-    plt.grid(True, linestyle='--', alpha=0.7)
-    plt.legend(fontsize=12)
+    rcParams['font.family'] = 'Times New Roman'
+    plt.ylim(0.5, 1)
+    plt.title(title, fontdict={'family': 'Times New Roman', 'size': 18})
+    plt.xlabel("Round", fontdict={'family': 'Times New Roman', 'size': 16})
+    plt.ylabel("Accuracy", fontdict={'family': 'Times New Roman', 'size': 16})
+    # plt.grid(True, linestyle='--', alpha=0.7)
+    plt.xticks(fontsize=14)    # fontsize of the tick labels
+    plt.yticks(fontsize=14)    # fontsize of the tick labels
+    plt.legend(fontsize=16,loc='lower left')
     plt.tight_layout()
 
     # Save the plot to a file
@@ -51,7 +56,7 @@ def plot_multiple(title,file_names):
 
 
 
-title = "SST2_Iid"
+title = "Square"
 folder_path = "./test_result"  # Replace with the path to your folder
 csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
 plot_multiple(title,csv_files)
